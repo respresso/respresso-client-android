b173520988d621f438f8ca821046378a879de413
Jozsef Mezei <jozsef.mezei@ponte.hu>
2019-09-04 14:19:37 +0200

    added animation to check button updated app style

--- example/app/src/main/java/hu/ponte/hellorespresso/MainActivity.kt
+++ example/app/src/main/java/hu/ponte/hellorespresso/MainActivity.kt
@@ -1,12 +1,26 @@
 package hu.ponte.hellorespresso
 
+import android.animation.ObjectAnimator
 import android.content.Intent
+import android.graphics.drawable.LayerDrawable
 import android.net.Uri
 import android.os.Bundle
 import hu.ponte.mobile.respresso.live_edit.Respresso
 import kotlinx.android.synthetic.main.activity_main.*
+import android.view.MotionEvent
+import android.animation.ValueAnimator
+import android.util.Log
+
 
 class MainActivity : BaseActivity() {
+
+    private var animator: ValueAnimator? = null
+    private var borderLeftTopPadding = -1
+    set(value) { if (field == -1) field = value; }
+    private var shapeRightPadding = -1
+        set(value) { if (field == -1) field = value; }
+    private var shapeBottomPadding = -1
+        set(value) { if (field == -1) field = value; }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +40,55 @@ class MainActivity : BaseActivity() {
         val webIntent = Intent(Intent.ACTION_VIEW)
         webIntent.data = Uri.parse(url)
 
-        button_check.setOnClickListener { startActivity(webIntent) }
+        button_check.setOnClickListener {
+            startActivity(webIntent)
+        }
+
+        button_check.setOnTouchListener { v, event ->
+            initAnim()
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> pressAnim()
+                MotionEvent.ACTION_UP -> releaseAnim()
+            }
+            false
+        }
+    }
+
+    private fun initAnim() {
+        val layer = button_check.background as? LayerDrawable
+        val shape = layer?.getDrawable(0)
+        val border = layer?.getDrawable(1)
+
+        val boundsShape = shape?.bounds
+        val boundsBorder = border?.bounds
+
+        if (boundsBorder == null || boundsShape == null) return
+
+        borderLeftTopPadding = boundsBorder.left
+        shapeRightPadding = boundsShape.right
+        shapeBottomPadding = boundsShape.bottom
+
+        animator = ObjectAnimator.ofInt(0, borderLeftTopPadding)
+        animator?.duration = 300
+
+        animator?.addUpdateListener {
+            val value = it.animatedValue as Int
+            border.setBounds(borderLeftTopPadding - value, borderLeftTopPadding - value, boundsBorder.right, boundsBorder.bottom)
+            shape.setBounds(boundsShape.left, boundsShape.top, shapeRightPadding + value, shapeBottomPadding + value)
+        }
+    }
+
+    private fun pressAnim() {
+        if (animator == null) initAnim()
+        animator?.start()
+    }
+
+    private fun releaseAnim() {
+        animator?.reverse()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        animator?.removeAllUpdateListeners()
     }
 }
